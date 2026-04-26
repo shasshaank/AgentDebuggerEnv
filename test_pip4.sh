df613c5fd79020e213059180c17ae524d71282f3
shank <shashaankjain07@gmail.com>
2026-04-26 10:37:31 +0530

    Revert "Fix: Dockerfile"
    This reverts commit bcd28bcea235f8a8c7ad26c9df886de8c44ab521.

--- test_pip4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 -m venv venv_test4
-source venv_test4/bin/activate
-pip install pip -U
-pip install datasets "huggingface-hub>=0.30" "hf-transfer>=0.1.4" "protobuf<4" "click<8.1"
-pip install -r req_test3.txt spaces
