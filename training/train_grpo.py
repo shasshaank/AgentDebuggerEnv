8c357de34d67ee2b41fde9a72d9d597cc9d2af5b
shank <shashaankjain07@gmail.com>
2026-04-26 10:24:31 +0530

    Fix: Fixed again again

--- training/train_grpo.py
+++ training/train_grpo.py
@@ -50,7 +50,7 @@ if not args.test_local:
         "accelerate==1.0.1",
         "trl==0.15.2",
         "peft==0.13.2",
-        "bitsandbytes==0.45.3",
+        "bitsandbytes==0.45.5",
     ]
     print("Installing training dependencies...", flush=True)
     ret = os.system(
